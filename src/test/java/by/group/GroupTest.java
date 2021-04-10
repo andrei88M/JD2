bfc5f5deffbc301ef393b0a8a672ae6dbdfed718
andrei88M <andrei1988mikhalchuk@gmail.com>
2021-04-10 11:54:31 +0300

    solved task person

---
+++ src/test/java/by/group/GroupTest.java
@@ -0,0 +1,11 @@
+package by.group;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class GroupTest extends TestCase {
+    @Test
+    public void testMath() {
+        assertTrue((int) (Math.random() * 7 + 1)>0);
+    }
+}
